d92c89de46e36285cc1b129ffee5be15aef91a47
Diego Bugs <diego.f.bugs@gmail.com>
2024-09-13 16:45:58 -0400

    Add OpenAI

--- index.js
+++ index.js
@@ -2,13 +2,16 @@
  * Entry point of the application
  */
 
-import * as core from "@actions/core";
+import { getInput, setFailed, info } from "@actions/core";
+import { getOctokit } from "@actions/github";
+import OpenAI from "openai";
 
 /**
  * @typedef {Object} ActionInputs
- * @property {string} [language] - The language (optional)
+ * @property {string} language - The language (defaults to "en")
  * @property {string} openaiApiKey - The OpenAI API key
  * @property {string} [token] - The token (optional)
+ * @property {string} [customPrompt] - The custom prompt (optional)
  */
 
 /**
@@ -16,9 +19,9 @@ import * as core from "@actions/core";
  * @returns {ActionInputs} The parsed action inputs
  */
 function parseInputs() {
-  const openaiApiKey = core.getInput("openai-api-key", { required: true });
-  const language = core.getInput("language");
-  const token = core.getInput("token");
+  const openaiApiKey = getInput("openai-api-key", { required: true });
+  const language = getInput("language");
+  const token = getInput("token");
 
   return {
     language,
@@ -30,9 +33,40 @@ function parseInputs() {
 /**
  * The main function that runs the action.
  */
-function run() {
-  core.info("Running ai release notes action");
-  const inputs = parseInputs();
+async function run() {
+  info("Running ai release notes action");
+  const { openaiApiKey, language, token } = parseInputs();
+  // const octokit = getOctokit(token);
+  //const release = context.payload.release;
+
+  const customPrompt = "This is a test prompt";
+
+  // Read PR merged related commits and PR body
+
+  try {
+    /* if (!release && !customPrompt) {
+      throw new Error("This action can only be run on pull requests");
+    } */
+
+    // Get the release notes
+    const releaseNotes = customPrompt; // release.body;
+    info(`Release notes: ${releaseNotes}`);
+
+    // Call the OpenAI API
+    const openai = new OpenAI({ apiKey: openaiApiKey });
+
+    const completion = await openai.chat.completions.create({
+      messages: [{ role: "system", content: "You are a helpful assistant." }],
+      model: "gpt-4o",
+    });
+
+    const response = completion.data.choices[0].message.content;
+    info(`Response: ${response}`);
+
+    // Create a comment on the pull request
+  } catch (error) {
+    setFailed(error.message || "Failed to run the action");
+  }
 }
 
 run();
